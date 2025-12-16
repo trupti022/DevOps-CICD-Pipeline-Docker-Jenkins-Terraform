03eb57d885fc9e3141229f45f15178f38806e82f
Trupti Desai-Porfolio <186296550+trupti022@users.noreply.github.com>
2025-12-16 17:39:06 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "devops_key" {
+  key_name   = "devops-key"
+  public_key = file("devops-key.pub")
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Allow SSH, Jenkins, App, K8s NodePort"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 85
+    to_port     = 85
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30085
+    to_port     = 30085
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins_ec2" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.devops_key.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+resource "aws_instance" "k8s_node" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.devops_key.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "Kubernetes-Node"
+  }
+}
